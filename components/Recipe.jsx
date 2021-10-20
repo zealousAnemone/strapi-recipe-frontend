3ca76752ee10a47be503c80d39fc24f63ca95faf
Veronica Stork <zealous.anemone@gmail.com>
2021-10-19 21:16:16 -0400

    Uses marked to display ingredients

--- components/Recipe.jsx
+++ components/Recipe.jsx
@@ -1,9 +1,15 @@
+import marked from 'marked';
+
 const Recipe = ({recipe}) => {
+  const getMarkdownText = (text) => {
+    const formattedText = marked(text); 
+    return { __html: formattedText};
+  }
 return (
   <div className='recipe-card'>
     <a href={recipe.link}><h3>{recipe.name}</h3></a>
     <img src="#"></img>
-    <div>{recipe.ingredients}</div>
+    <div dangerouslySetInnerHTML={getMarkdownText(recipe.ingredients)}></div>
     <div>{recipe.instructions}</div>
 
     <div>{recipe.type.type}</div>
